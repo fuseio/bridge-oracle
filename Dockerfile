cdfa666dd793041c6a450e0af7e6987e52186cfd
Leon Prouger <leonprou@gmail.com>
2019-05-29 13:59:32 +0300

    Fixing e2e tests

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:8
 
 # https://superuser.com/questions/1423486/issue-with-fetching-http-deb-debian-org-debian-dists-jessie-updates-inrelease
-RUN printf "deb http://archive.debian.org/debian/ jessie main\ndeb-src http://archive.debian.org/debian/ jessie main\ndeb http://security.debian.org jessie/updates main\ndeb-src http://security.debian.org jessie/updates main" > /etc/apt/sources.list
+#RUN printf "deb http://archive.debian.org/debian/ jessie main\ndeb-src http://archive.debian.org/debian/ jessie main\ndeb http://security.debian.org jessie/updates main\ndeb-src http://security.debian.org jessie/updates main" > /etc/apt/sources.list
 
 RUN apt-get update
 RUN apt-get install -y build-essential
--- e2e/run-tests.sh
+++ e2e/run-tests.sh
@@ -1,18 +1,24 @@
 docker-compose up -d --build
+
 docker-compose run e2e npm run deploy
+
 docker-compose run -d bridge npm run watcher:signature-request
 docker-compose run -d bridge npm run watcher:collected-signatures
 docker-compose run -d bridge npm run watcher:affirmation-request
+
 docker-compose run -d bridge-erc npm run watcher:signature-request
 docker-compose run -d bridge-erc npm run watcher:collected-signatures
 docker-compose run -d bridge-erc npm run watcher:affirmation-request
+
 docker-compose run -d bridge-erc-native npm run watcher:signature-request
 docker-compose run -d bridge-erc-native npm run watcher:collected-signatures
 docker-compose run -d bridge-erc-native npm run watcher:affirmation-request
+
 docker-compose run -d bridge-erc-multiple npm run watcher:signature-request
 docker-compose run -d bridge-erc-multiple npm run watcher:collected-signatures
 docker-compose run -d bridge-erc-multiple npm run watcher:affirmation-request
-docker-compose run -d bridge-erc-multiple npm run watcher:bridge-deployed
+docker-compose run --name bridge_deployed -d bridge-erc-multiple npm run watcher:bridge-deployed
+
 docker-compose run -d bridge npm run sender:home
 docker-compose run -d bridge npm run sender:foreign
 docker-compose run e2e npm start
